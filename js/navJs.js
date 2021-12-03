60113b8c5ec971e296e6f15284f833706760506d
AjiThHubgit <88279367+AjiThHubgit@users.noreply.github.com>
2021-12-03 23:22:07 +0530

    Initial comment

---
+++ js/navJs.js
@@ -0,0 +1,12 @@
+
+window.addEventListener('click', function(e){
+	if (document.getElementById('searchInputBox').contains(e.target)){
+        document.getElementById("mhpVr").style.display = "none";
+        document.getElementById("mhpNav").style.display = "none";
+        document.getElementById("searchExtend").classList.add("w-100");
+  } else{
+    document.getElementById("mhpVr").style.display = "flex";
+    document.getElementById("mhpNav").style.display = "flex";
+    document.getElementById("searchExtend").classList.remove("w-100");
+  }
+})
