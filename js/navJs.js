a52f3400dd07afa2d61e64ffed198d77dc7b14ab
AjiThHubgit <88279367+AjiThHubgit@users.noreply.github.com>
2021-12-12 14:09:32 +0530

    Header Search issue and card issue fixed

--- js/navJs.js
+++ js/navJs.js
@@ -1,13 +1,19 @@
 
 window.addEventListener('click', function(e){
 	if (document.getElementById('searchInputBox').contains(e.target)){
-        document.getElementById("mhpVr").style.display = "none";
-        document.getElementById("mhpNav").style.display = "none";
-        document.getElementById("searchExtend").classList.add("w-100");
+    if(this.screen.width > 1024)
+    {
+      document.getElementById("mhpVr").style.display = "none";
+      document.getElementById("mhpNav").style.display = "none";
+      document.getElementById("searchExtend").classList.add("w-100");
+
+      return
+    }
+
   } else{
     document.getElementById("mhpVr").style.display = "flex";
     document.getElementById("mhpNav").style.display = "flex";
     document.getElementById("searchExtend").classList.remove("w-100");
     document.getElementById("mhpVr").classList.remove("d-none");
   }
-})
+});
