552adacd53315ccd475677619d563b2d5f9eb941
Sumat22 <sumathidharmaraj@gmail.com>
2021-12-18 18:36:11 +0530

    top menu changes updated

--- js/navJs.js
+++ js/navJs.js
@@ -1,19 +1,20 @@
 
 window.addEventListener('click', function(e){
 	if (document.getElementById('searchInputBox').contains(e.target)){
-    if(this.screen.width > 768)
+    if(document.body.clientWidth > 1200)
     {
       document.getElementById("mhpVr").style.display = "none";
       document.getElementById("mhpNav").style.display = "none";
       document.getElementById("searchExtend").classList.add("w-100");
-
       return
     }
-
-  } else{
-    document.getElementById("mhpVr").style.display = "flex";
-    document.getElementById("mhpNav").style.display = "flex";
-    document.getElementById("searchExtend").classList.remove("w-100");
-    document.getElementById("mhpVr").classList.remove("d-none");
   }
+    else
+    {
+      document.getElementById("mhpVr").style.display = "none";
+      document.getElementById("mhpNav").style.display = "flex";
+      document.getElementById("searchExtend").classList.remove("w-100");
+      //document.getElementById("mhpVr").classList.remove("d-none");
+    }
+
 });
